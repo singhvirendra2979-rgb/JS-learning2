4eae324b6ba5d77623f7e3dc015be336924fbbee
singhvirendra2979-rgb <singhvirendra2979@gmail.com>
2026-07-23 13:18:37 +0000

    variables&datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,29 @@
+const accountId = 23456
+let Email = "aman@gmail"
+var Password = "jaipur"
+city = "kota"
+
+//accountId = 80u4892   (not allowed)
+
+
+
+
+
+Email = "ramlal@gmail"
+Password = "73984747702"
+city = "bengalaur"
+
+
+/*
+Prefer not to use var 
+because of issue  in block scope and functional scope 
+
+
+*/
+
+
+
+
+console.log(accountId);
+
+console.table([accountId,Email,Password,city])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser.
+
+
+console.log("hitesh")
+
+
+
+let name = "aman"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+//number => 2 to the powe 53
+//bigint
+//string => ""
+//boolean => true/false 
+//null => standalone value
+//undefined =>   
+    //symbol => unique
+
+
+
+
+
+
+//object 
+
+
+console.log(typeof "hitesh") //string
+console.log(typeof null);   // object
+console.log(typeof undefined) //undefined
+
